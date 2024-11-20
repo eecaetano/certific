5751d6731b65570d44c0d6be6dd65cd846482411
ANDERSON R OLIVEIRA <103950004+eecaetano@users.noreply.github.com>
2024-11-20 20:39:56 -0300

    Update consultar-certificado-digital.js

--- consultar-certificado-digital.js
+++ consultar-certificado-digital.js
@@ -1,7 +1,3 @@
-    /** chama funcao que valida documento */
-            $('#btn-consultar').click(function () {
-                window.location.href = 'https://drive.google.com/file/d/1pl7wf-eAQmfGiHplBXit1sbwRhmP2Qgd/view?usp=drive_link';
-            });
 /** chama funcao que valida documento */
 $('#btn-consultar').click(function () {
     window.location.href = 'https://drive.google.com/file/d/1pl7wf-eAQmfGiHplBXit1sbwRhmP2Qgd/view?usp=drive_link';
