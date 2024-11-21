78218f52b9d84f4eaa88b41f09a91ff9cf66c938
ANDERSON R OLIVEIRA <103950004+eecaetano@users.noreply.github.com>
2024-11-21 18:58:41 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-consultar').addEventListener('click', function() {
-        var docRegistro = document.getElementById('docRegistro').value;
-        var chave = document.getElementById('chave').value;
+    const btnConsultar = document.getElementById('btn-consultar');
+    const docRegistroInput = document.getElementById('docRegistro');
+    const chaveInput = document.getElementById('chave');
 
-        if (docRegistro === '299.413.308-32' && chave === 'E8281C7D-71FBE3FF-8C8F0B1A-0B713877') {
-            window.open('https://drive.google.com/file/d/1pl7wf-eAQmfGiHplBXit1sbwRhmP2Qgd/view?usp=drive_link', '_blank');
-        } else {
-            alert('Documento de Registro ou Chave inválidos.');
-        }
-    });
+    if (btnConsultar && docRegistroInput && chaveInput) {
+        btnConsultar.addEventListener('click', function() {
+            const docRegistro = docRegistroInput.value;
+            const chave = chaveInput.value;
+
+            if (docRegistro === '299.413.308-32' && chave === 'E8281C7D-71FBE3FF-8C8F0B1A-0B713877') {
+                window.open('https://drive.google.com/file/d/1pl7wf-eAQmfGiHplBXit1sbwRhmP2Qgd/view?usp=drive_link', '_blank');
+            } else {
+                alert('Documento de Registro ou Chave inválidos.');
+            }
+        });
+    } else {
+        console.error('Elementos necessários não encontrados na página.');
+    }
 });
